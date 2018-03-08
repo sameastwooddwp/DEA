83cb25acb75fcef0b150a06a598e85801db6d127
Sam Eastwood <sameastwood.digitaldwp@gmail.com>
2018-03-08 16:53:34 +0000

    DEA

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+/* global $ */
+/* global GOVUK */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+  // with role="button" when the space key is pressed.
+  GOVUK.shimLinksWithButtonRole.init()
+
+  // Details/summary polyfill from frontend toolkit
+  GOVUK.details.init()
+
+  // Show and hide toggled content
+  // Where .multiple-choice uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+})
+
+
+
+$('.js-upload').change(function() {
+    var filename = $(this).val();
+    if (filename.substring(3,11) == 'fakepath') {
+      filename = filename.substring(12);
+    }
+    $('.js-upload_filename').html(filename).show();
+});
